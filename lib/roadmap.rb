f84f3d60215836fd85e11a6c0206f88915c60475
Jacob <jake.laurenti@gmail.com>
2017-12-15 09:43:48 -0500

    Checkpoint Submission should be working

--- lib/roadmap.rb
+++ lib/roadmap.rb
@@ -5,6 +5,7 @@ module Roadmap
   include HTTParty
 
   def get_roadmap(roadmap_id)
+  # my roadmap_id is 37
     response = self.class.get(api_url("roadmaps/#{roadmap_id}"), headers:{ "authorization" => @auth_token})
     @roadmap = JSON.parse(response.body)
   end
@@ -13,6 +14,21 @@ module Roadmap
     response = self.class.get(api_url("checkpoints/#{checkpoint_id}"), headers:{ "authorization" => @auth_token})
     @checkpoint = JSON.parse(response.body)
   end
+
+  def create_submission(checkpoint_id, assignment_branch, assignment_commit_link, comment, enrollment_id)
+  # my id 24534
+  # this checkpoint 2162
+  response = self.class.post(api_url("checkpoint_submissions"),
+             body: { checkpoint_id: checkpoint_id, assignment_branch: assignment_branch,
+                     assignment_commit_link: assignment_commit_link, comment: comment, enrollment_id: enrollment_id  },
+             headers: { "authorization" => @auth_token })
+    if response.success?
+      puts "checkpoint submitted"
+    else
+      puts "checkpoint failed to submit"
+    end
+  end
+
 
   private
 
